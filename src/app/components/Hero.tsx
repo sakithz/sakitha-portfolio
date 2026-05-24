8b8879def498cb6f5758ebb15eb8133f77bdf536
sakithz <sakithapalliyag@gmail.com>
2026-05-24 08:35:33 +0530

    portfolio

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -257,7 +257,7 @@ export function Hero() {
                             duration: 1,
                             delay: 0.1,
                         }}
-                        className="text-[3.5rem] leading-[0.88] font-bold tracking-[-0.07em] uppercase text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-white"
+                        className="text-[4rem] leading-[0.9] font-bold tracking-[-0.06em] uppercase text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-white"
                     >
                         PALLIYAGURU
                     </motion.h1>
